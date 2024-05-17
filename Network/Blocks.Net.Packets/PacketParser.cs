1b68d6b1fc892efbb04293db978dc1819606cb6d
Lexi <arallen3660@gmail.com>
2024-05-17 14:29:45 -0400

    Actually read packets

--- Network/Blocks.Net.Packets/PacketParser.cs
+++ Network/Blocks.Net.Packets/PacketParser.cs
@@ -11,31 +11,31 @@ public static partial class PacketParser
     {
         _ = VarInt.ReadFrom(stream);
         var id = VarInt.ReadFrom(stream);
-        // if (HandshakingServerBoundPackets.TryGetValue(id, out var cons)) return cons(stream);
-        throw new Exception($"Unsupported Handshaking Packet ID: {id}");
+        if (HandshakingServerBoundPackets.TryGetValue(id, out var cons)) return cons(stream);
+        throw new Exception($"Unsupported Handshaking Packet ID: {id.Value}");
     }    
     
     public static IPacket ParseStatus(MemoryStream stream)
     {
         _ = VarInt.ReadFrom(stream);
         var id = VarInt.ReadFrom(stream);
-        // if (StatusServerBoundPackets.TryGetValue(id, out var cons)) return cons(stream);
-        throw new Exception($"Unsupported Status Packet ID: {id}");
+        if (StatusServerBoundPackets.TryGetValue(id, out var cons)) return cons(stream);
+        throw new Exception($"Unsupported Status Packet ID: {id.Value}");
     }
     
     public static IPacket ParseLogin(MemoryStream stream)
     {
         _ = VarInt.ReadFrom(stream);
         var id = VarInt.ReadFrom(stream);
-        // if (LoginServerBoundPackets.TryGetValue(id, out var cons)) return cons(stream);
-        throw new Exception($"Unsupported Login Packet ID: {id}");
+        if (LoginServerBoundPackets.TryGetValue(id, out var cons)) return cons(stream);
+        throw new Exception($"Unsupported Login Packet ID: {id.Value}");
     }
     
     public static IPacket ParseConfiguration(MemoryStream stream)
     {
         _ = VarInt.ReadFrom(stream);
         var id = VarInt.ReadFrom(stream);
-        // if (ConfigurationServerBoundPackets.TryGetValue(id, out var cons)) return cons(stream);
-        throw new Exception($"Unsupported Configuration Packet ID: {id}");
+        if (ConfigurationServerBoundPackets.TryGetValue(id, out var cons)) return cons(stream);
+        throw new Exception($"Unsupported Configuration Packet ID: {id.Value}");
     }
 }
